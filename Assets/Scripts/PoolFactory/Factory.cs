6ca0c8b2e3ab9b33d85c38a1f9800fd3e10a35ce
Nico <nicolas.migues97@gmail.com>
2026-05-01 19:41:23 -0300

    InitFactoryPool

--- Assets/Scripts/PoolFactory/Factory.cs
+++ Assets/Scripts/PoolFactory/Factory.cs
@@ -1,16 +1,5 @@
 using UnityEngine;
-
-public class Factory : MonoBehaviour
+public class Factory <T> where T : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
