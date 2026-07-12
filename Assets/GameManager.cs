1caf7e87fcbca80d1a20a0245105d720bbcf0f9a
Nico <nicolas.migues97@gmail.com>
2026-07-12 18:53:52 -0300

    OKPreLvlDesing

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,12 +3,12 @@ public class GameManager : MonoBehaviour
 {
     public Player player;
     public static GameManager instance;
-    public Transform _projectilesParent;
+    public Transform projectilesParent;
     private void Awake()
     {
         if (instance == null)
             instance = this;
         else
-            Destroy(this);
+            Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/Enemy/Spawners/EnemySpawnerManager.cs
+++ Assets/Scripts/Enemy/Spawners/EnemySpawnerManager.cs
@@ -17,7 +17,7 @@ public abstract class EnemySpawnerManager : MonoBehaviour, IObserver<EnemyEvent>
         // CreateShootStrategy() (que corre adentro de esa linea) lo necesita.
         if (_impactEffectPrefab != null)
         {
-            ImpactEffectService impactEffectService = new ImpactEffectService(_impactEffectPrefab, GameManager.instance._projectilesParent, _impactEffectPoolSize);
+            ImpactEffectService impactEffectService = new ImpactEffectService(_impactEffectPrefab, GameManager.instance.projectilesParent, _impactEffectPoolSize);
             BulletImpactCallback = impactEffectService.PlayAt;
         }
         _enemyService = new EnemyService(_enemyPrefab, transform, GetPoolSize(), this, CreateShootStrategy(), GameManager.instance.player.transform);
