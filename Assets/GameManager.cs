a1527f586f390611035bd8da301ecb52cefde89f
Nico <nicolas.migues97@gmail.com>
2026-07-15 01:10:51 -0300

    OKSomeFeatures

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,8 +7,6 @@ public class GameManager : MonoBehaviour
     public Player player;
     public static GameManager instance;
     public Transform projectilesParent;
-    //score la hago constante simplente porque la Key no va a cambiar pero la realidad es que nos se llama fuera de gameManager
-    private const string ScoreKey = "Score";
     //Una const pertence a la clase no a la instancia y yo desde el Menu quiero acceder a esta variable
     //por eso la hago Const
     //como el Menu no tiene game manager por eso necesito que sea constante esta varibale
@@ -22,12 +20,12 @@ public class GameManager : MonoBehaviour
             instance = this;
         else
             Destroy(gameObject);
-        _score = PlayerPrefs.GetInt(ScoreKey, 0);
         //Se active cuando muere un enemigo en EnemySpawnerManager
         EventManager.SubscribeToEvent(EventType.EnemyKilled, OnEnemyKilled);
     }
     private void Start()
     {
+        _score = 0;
         //El unico que se suscbir al evento ScoreChanged es la UI
         EventManager.TriggerEvent(EventType.ScoreChanged, _score);
     }
@@ -38,17 +36,9 @@ public class GameManager : MonoBehaviour
     public void AddScore(int amount)
     {
         _score += amount;
-        PlayerPrefs.SetInt(ScoreKey, _score);
         // Actualizo el record historico si supere el maximo.
         if (_score > PlayerPrefs.GetInt(HighScoreKey, 0))
             PlayerPrefs.SetInt(HighScoreKey, _score);
-        EventManager.TriggerEvent(EventType.ScoreChanged, _score);
-    }
-    //Metodo por si algun dia necesito reiniciar el score.
-    public void ResetScore()
-    {
-        _score = 0;
-        PlayerPrefs.SetInt(ScoreKey, _score);
         EventManager.TriggerEvent(EventType.ScoreChanged, _score);
     }
     private void OnDestroy()
